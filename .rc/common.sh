0e0a7e409c3863094a3466f161b8a94df8ac29a3
Jerry Green <jerrygreenest@gmail.com>
2020-11-23 07:32:40 +0500

    Edit comments

--- .rc/common.sh
+++ .rc/common.sh
@@ -1,17 +1,17 @@
-# Aliases
+# My default aliases:
 alias projects="cd ~/projects"
 
-# Completions (feel free to add bash completions there, this folder has no permission errors across all platforms)
+# Use completions (feel free to add bash completions there, this folder has no permission errors across all platforms):
 [[ -d ~/.completions && -n $(ls -A ~/.completions) ]] && . ~/.completions/*
 
-# Bash history to be a good size
+# Bash history to be a good size:
 HISTSIZE=1000000
 HISTFILESIZE=1000000
 
-# Disable editing for git
+# Use default merge message when merging:
 export GIT_MERGE_AUTOEDIT=no
 
-# TODO: probably remove functions
+# TODO: probably remove functions?
 function killport() {
   kill -9 $(lsof -t -i:$1)
 }
