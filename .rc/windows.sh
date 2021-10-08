bddfc1548f185c5468cb29bdf455544b35368ca7
Jerry Green <jerrygreenest@gmail.com>
2021-10-09 00:29:57 +0500

    Update windows.sh: Add Cygwin setting for symlinks
    Make symlinks symlinks again!

--- .rc/windows.sh
+++ .rc/windows.sh
@@ -1,2 +1,3 @@
 # Fixes
 export PROMPT_COMMAND='history -a' # Fixes bug with history not filling up
+export CYGWIN="winsymlinks:nativestrict" # makes Cygwin create actual Windows symlinks with `ln` command, rather than making a copy (Cygwin, why this isn't default?)
