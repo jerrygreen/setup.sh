56e47a3c0708385ba21b6bd3c9048cc25abc155d
Jerry Green <me@jerrygreen.me>
2021-10-10 07:29:48 +0500

    Update windows.sh

--- .rc/windows.sh
+++ .rc/windows.sh
@@ -1,6 +1,6 @@
 # Fixes bug with history not filling up:
 export PROMPT_COMMAND='history -a'
 
-# Makes Cygwin create actual Windows symlinks with `ln` command, rather than making a copy (Cygwin, why this isn't default?):
-# export CYGWIN=winsymlinks:nativestrict # not needed???
+# Makes Cygwin create actual Windows symlinks with `ln -s` command, rather than making a copy (Cygwin, why this isn't default?):
+export CYGWIN=winsymlinks:nativestrict
 export MSYS=winsymlinks:nativestrict
