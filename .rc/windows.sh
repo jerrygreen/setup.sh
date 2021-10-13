c2a1559a293c6900bb6a17af0eea85e1a1794bc8
Jerry Green <me@jerrygreen.me>
2021-10-13 12:01:03 +0500

    Update windows.sh

--- .rc/windows.sh
+++ .rc/windows.sh
@@ -1,8 +1,8 @@
-# Fix History: so it's filling up during crashes (how this even a bug?): https://github.com/microsoft/vscode/issues/85831
+# Fix history: so it's filling up during crashes (how this even a bug?): https://github.com/microsoft/vscode/issues/85831
 export PROMPT_COMMAND='history -a'
 
-# Fix Symlinks: `ln -s` bash command to actually create symbolic links, rather than making a copy (Cygwin, why this isn't default?):
+# Fix symlinks: `ln -s` bash command to actually create symbolic links, rather than making a copy (Cygwin, why this isn't default?):
 export MSYS=winsymlinks:nativestrict
 
-# Do not write duplicate commands, and commands starting with space, into history:
+# Fix shell: disable writing duplicate commands and commands starting with space, into history:
 export HISTCONTROL=erasedups:ignorespace:ignoredups
