eb0b92efca609ad07f02ae6a944bd4a06aafb25c
Jerry Green <me@jerrygreen.me>
2021-10-13 12:01:40 +0500

    Update macos.sh

--- .rc/macos.sh
+++ .rc/macos.sh
@@ -1,7 +1,7 @@
-# Disable "Dock" menu bouncing (which is annoying):
+# Fix "Dock" menu: disable bouncing (which is annoying):
 defaults write com.apple.dock no-bouncing -bool TRUE
 
-# Do not write duplicate commands, and commands starting with space, into history:
+# Fix shell: disable writing duplicate commands and commands starting with space, into history:
 setopt HIST_IGNORE_ALL_DUPS      # Delete old recorded entry if new entry is a duplicate.
 setopt HIST_FIND_NO_DUPS         # Do not display a line previously found.
 setopt HIST_IGNORE_SPACE         # Don't record an entry starting with a space.
