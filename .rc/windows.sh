077c520c740b7544e76c5018ae9d8d23a293d725
Jerry Green <me@jerrygreen.me>
2021-10-13 10:40:06 +0500

    Update windows.sh

--- .rc/windows.sh
+++ .rc/windows.sh
@@ -3,3 +3,6 @@ export PROMPT_COMMAND='history -a'
 
 # Fix Symlinks: `ln -s` bash command to actually create symbolic links, rather than making a copy (Cygwin, why this isn't default?):
 export MSYS=winsymlinks:nativestrict
+
+# Do not write duplicate commands, and commands starting with space, into history:
+export HISTCONTROL=erasedups:ignorespace:ignoredups
