e7cf42d5e667b3171be876c8b6c35ffd930cb55f
Jerry Green <me@jerrygreen.me>
2022-02-14 03:53:02 +0500

    Update git.sh

--- rc-once/git.sh
+++ rc-once/git.sh
@@ -1,2 +1,5 @@
 # Push git tags automatically: no more `git push --tags`!
 git config --global push.followTags true
+
+# Forever solve the warning "LF will be replaced by CRLF": https://stackoverflow.com/a/5834094/3720305
+git config core.autocrlf true
