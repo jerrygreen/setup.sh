20755b782021c5fa96d8ac7d0f815e99463345ee
Jerry Green <jerrygreenest@gmail.com>
2022-12-29 12:27:26 +0500

    Update git.sh

--- rc-once/git.sh
+++ rc-once/git.sh
@@ -2,4 +2,8 @@
 git config --global push.followTags true
 
 # Forever solve the warning "LF will be replaced by CRLF": https://stackoverflow.com/a/5834094/3720305
-git config core.autocrlf true
+git config --global core.autocrlf true
+
+# Automatically scroll status if needed (like git log) & shortify its output
+git config --global pager.status true
+git config --global status.short true
