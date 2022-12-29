ead32dde5df561328b589a5988a9325cbfa82949
Jerry Green <jerrygreenest@gmail.com>
2022-12-29 14:28:26 +0500

    Use author date instead of committer date
    More info:
    https://git-scm.com/docs/pretty-formats

--- rc-once/git.sh
+++ rc-once/git.sh
@@ -9,4 +9,4 @@ git config --global pager.status true
 git config --global status.short true
 
 # `git lg` - alternative git log for short: https://stackoverflow.com/a/36731148/3720305
-git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --abbrev-commit"
