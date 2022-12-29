f27f7c16d6a2147aec9b1cb5733edaf136897001
Jerry Green <jerrygreenest@gmail.com>
2022-12-29 14:15:17 +0500

    Update git.sh

--- rc-once/git.sh
+++ rc-once/git.sh
@@ -7,3 +7,6 @@ git config --global core.autocrlf true
 # Automatically scroll status if needed (like git log) & shortify its output
 git config --global pager.status true
 git config --global status.short true
+
+# `git lg` - alternative git log for short: https://stackoverflow.com/a/36731148/3720305
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
