d9533d049715368f9b1f9cfc698433f0126d6a33
Jerry Green <jerrygreenest@gmail.com>
2022-12-29 14:37:29 +0500

    Update git.sh
    Status too short, it seems. Doesn't show commit difference from rebase.

--- rc-once/git.sh
+++ rc-once/git.sh
@@ -4,9 +4,8 @@ git config --global push.followTags true
 # Forever solve the warning "LF will be replaced by CRLF": https://stackoverflow.com/a/5834094/3720305
 git config --global core.autocrlf true
 
-# Automatically scroll status if needed (like git log) & shortify its output
+# Automatically scroll status if needed (like git log)
 git config --global pager.status true
-git config --global status.short true
 
 # `git lg` - alternative git log for short: https://stackoverflow.com/a/36731148/3720305
 git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%ar) %C(bold blue)<%an>%Creset' --abbrev-commit"
