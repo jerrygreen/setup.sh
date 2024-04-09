a9e18e6acf4df6d10e4d49da3b5b9dc8f7438fd1
Jerry Green <jerrygreenest@gmail.com>
2024-04-09 20:17:17 +0500

    `npm config set` changed

--- rc-once/npm&yarn.sh
+++ rc-once/npm&yarn.sh
@@ -1,3 +1,3 @@
 # Disable save-prefix in order to set strict versions for higher reliability from errors through years
-npm config set save-prefix=""
+npm config set save-exact=true -g
 yarn config set save-prefix ""
