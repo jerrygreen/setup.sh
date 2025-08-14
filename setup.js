d9c47824faea248da7c245643f034677a0999adf
Jerry Green <jerrygreenest@gmail.com>
2025-08-14 09:21:46 +0500

    Update setup.js

--- setup.js
+++ setup.js
@@ -14,10 +14,10 @@ const HOME_FILES = ['.bunfig.toml']
 const CONF_FILES = ['nu/env.nu', 'nu/config.nu']
 // const RC_FILES = ['rc/aliases.nu']
 const RCO_FILES = ['rco/git.sh', 'rco/npm.sh']
-const HOME = os.homedir()
+// const HOME = os.homedir()
 
 const CMD = `$nu.default-config-dir | path join 'rc.nu'`
-// const HOME = execSync(`${NU_PATH} -c "${S}$nu.HOME"`).toString().trim()
+const HOME = execSync(`${NU_PATH} -c "${S}$nu.HOME"`).toString().trim()
 const RC_FILE = execSync(`${NU_PATH} -c "${S}${CMD}"`).toString().trim()
 const ENV_PATH = execSync(`${NU_PATH} -c "${S}$nu.env-path"`).toString().trim()
 const CONF_PATH = execSync(`${NU_PATH} -c "${S}$nu.config-path"`).toString().trim()
